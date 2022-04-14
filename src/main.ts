12997eef455763e3a86cf942fd50a2762ba4a760
Tim Sankov <tymofii.sankov@wtt-solutions.com>
2022-04-14 17:26:35 +0300

    install swagger

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -11,6 +12,14 @@ async function bootstrap() {
     AppModule,
     new FastifyAdapter(),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Hosusetable api')
+    .setVersion('1.0')
+    .addTag('hosusetable')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
